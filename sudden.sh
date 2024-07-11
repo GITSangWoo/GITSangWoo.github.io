da9a4a83571745eb1d8c7b45709c65d3dd1fc651
GITSangWoo <dogcenta@gmail.com>
2024-07-11 11:27:37 +0900

    done

--- sudden.sh
+++ sudden.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 FILE=~/tmp/_SUDDEN
+DATE=$(date +"%Y%m%d %H:%M:%S")
 
 while true
 do
+DATE=$(date +"%Y%m%d %H:%M:%S")
 	if [[ -f "$FILE" ]]; then
 	   #echo "check $FILE"
 	   echo "SUDDEN!!!"
    	else
 	     clear
 	     figlet KIA	
-     	     date '+%Y-%m-%d'	     
+     	     echo $DATE	     
 	fi
 	sleep 3
 done
