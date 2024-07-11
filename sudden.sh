0224251388ac3711295f90123d6495c33d59fed7
GITSangWoo <dogcenta@gmail.com>
2024-07-11 10:08:41 +0900

    dev deploy

--- sudden.sh
+++ sudden.sh
@@ -4,7 +4,13 @@ FILE=~/tmp/_SUDDEN
 
 while true
 do
-	echo "check"
+	if [[ -f "$FILE" ]]; then
+	   #echo "check $FILE"
+	   echo "SUDDEN!!!"
+   	else
+	     clear
+     	     date '+%Y-%m-%d'	     
+	fi
 	sleep 3
 done
 
